06003bdeedcb5dbed8b3444d52d6b7de80c0b267
aziz <chnitiaziz@gmail.com>
2025-11-10 17:20:53 +0100

    Fix Dockerfile for correct paths and ESM imports

--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login,getCurrentUser } from "../controllers/authController.js";
+import { register, login,getCurrentUser } from "../controllers/authcontroller.js";
 import { verifyToken, isAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
