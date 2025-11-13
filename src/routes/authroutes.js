a846ef6e58904bfc2ac667ae2ddc058eed0f84d3
aziz <chnitiaziz@gmail.com>
2025-11-13 14:04:00 +0100

    register unprotected

--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -5,8 +5,8 @@ import { verifyToken, isAdmin } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 // Only admins can register new users
-router.post("/register", verifyToken, isAdmin, register);
-
+//router.post("/register", verifyToken, isAdmin, register);
+router.post("/register", register);
 // Anyone can log in
 router.post("/login", login);
 router.get("/me", verifyToken, getCurrentUser);
